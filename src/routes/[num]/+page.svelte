fb86c57d1662d5c3d7cd80beb4e0269ae64c20ec
David Kouril <david.kouril@gmail.com>
2024-02-03 19:56:59 -0500

    more routes

--- src/routes/[num]/+page.svelte
+++ src/routes/[num]/+page.svelte
@@ -1,1 +1,14 @@
-TEST
+<script lang="ts">
+  import { Canvas } from '@threlte/core'
+  import Scene from '$lib/components/Scene.svelte'
+
+  import type { PageData } from './$types';
+          
+  export let data: PageData;
+  
+</script>
+
+  TEST: { data.num }
+<!-- <Canvas> -->
+<!--   <Scene num={100} showLinks={true}  /> -->
+<!-- </Canvas> -->
