c0eebaaad2590f59d0e2f2c57a416fbbb2269d15
David Kouril <david.kouril@gmail.com>
2024-02-04 15:17:28 -0500

    also links works

--- src/routes/[num]/[links]/+page.svelte
+++ src/routes/[num]/[links]/+page.svelte
@@ -1,11 +1,24 @@
 <script lang="ts">
+  import App from '$lib/components/App.svelte'
+
   import type { PageData } from './$types';
           
   export let data: PageData;
 </script>
 
-TEST: { data.num }, { data.links }
+<div>
+  <App numOfSpheres={data.num} showLinks={data.links} />
+</div>
 
-<!-- <Canvas> -->
-<!--   <Scene num={data.num} showLinks={data.links}  /> -->
-<!-- </Canvas> -->
+<style>
+  :global(body) {
+    margin: 0;
+  }
+
+  div {
+    width: 100vw;
+    height: 100vh;
+    background: rgb(13, 19, 32);
+    background: linear-gradient(180deg, rgba(13, 19, 32, 1) 0%, rgba(8, 12, 21, 1) 100%);
+  }
+</style>
